ccaaab24887561d239961f95e32e6cd56b85f614
en3rypt <sanjithnathan@gmail.com>
2023-03-08 21:54:58 +0530

    draggable UI

--- src/components/Commands.jsx
+++ src/components/Commands.jsx
@@ -1,12 +1,18 @@
 import React,{useRef} from 'react'
 import Draggable from 'react-draggable';
+
 function Commands() {
     const nodeRef = useRef(null);
   return (
     <Draggable  nodeRef={nodeRef}>
-        <div  ref={nodeRef} className='w-40 h-40 right-5 top-20 bg-slate-300 bg-opacity-90 absolute cursor-move  rounded-md'>
-            <div>
-                hello world
+        <div  ref={nodeRef} className='px-5 py-5 w-fit right-5 top-20 bg-slate-300 bg-opacity-90 absolute cursor-move  rounded-md'>
+            <div className='flex justify-center items-center'>
+                <button  class="mx-2 px-4 py-2 text-xs md:text-base font-medium md:leading-6 text-white whitespace-no-wrap bg-red-600 border border-red-700 rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500" data-rounded="rounded-md" data-primary="red-600" data-primary-reset="{}">
+                    Clear Walls
+                </button>
+                <button  class="mx-2 px-4 py-2 text-xs md:text-base font-medium md:leading-6 text-white whitespace-no-wrap bg-green-600 border border-green-700 rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500" data-rounded="rounded-md" data-primary="green-600" data-primary-reset="{}">
+                    Visualize
+                </button>
             </div>
         </div>
     </Draggable>
