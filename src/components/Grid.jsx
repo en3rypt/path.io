450b62854eff7309cf5826ef34786ee76d669416
jassuwu <20pw14@psgtech.ac.in>
2023-03-09 10:21:44 +0530

    applied BFS to the grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,47 +1,51 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 //components
 
 
 function Grid(props) {
   const [mouseDown, setMouseDown] = useState(false)
-  function handleMouseDown(node){
-    if(node.isStartNode || node.isEndNode) return;
+  function handleMouseDown(node) {
+    if (node.isStartNode || node.isEndNode) return;
     node.isWall = !node.isWall
     setMouseDown(true)
   }
-  function handleMouseUp(){
+  function handleMouseUp() {
     setMouseDown(false)
   }
-  function handleMouseOver(node){
+  function handleMouseOver(node) {
     if (!mouseDown) return;
-      props.setGrid({
-        ...props.grid,
-        nodes: props.grid.map((row,rowIndex)=>{
-          return row.map((col,colIndex)=>{
-            if(rowIndex === node.x && colIndex === node.y && node.isWall === false && !node.isStartNode && !node.isEndNode ){
-              return {
-                ...col,
-                isWall: !col.isWall
-              }
+    props.setGrid({
+      ...props.grid,
+      nodes: props.grid.map((row, rowIndex) => {
+        return row.map((col, colIndex) => {
+          if (rowIndex === node.x && colIndex === node.y && node.isWall === false && !node.isStartNode && !node.isEndNode) {
+            return {
+              ...col,
+              isWall: !col.isWall
             }
-            return col
-          })
+          }
+          return col
         })
       })
-    
+    })
+
   }
-  function getClassName(node){
+  function getClassName(node) {
     var classname = "w-[40px] h-[40px] outline outline-1 outline-slate-900 "
-    if(node.isStartNode){
+    if (node.isStartNode) {
       classname += "bg-green-500 animate-wallAnimation"
     }
-    if(node.isEndNode){
+    if (node.isEndNode) {
       classname += "bg-red-500 animate-wallAnimation"
     }
-    if(node.isWall){
+    if (node.isWall) {
       classname += "bg-stone-500  animate-wallAnimation"
     }
+    if (node.isVisited) {
+      classname += "bg-cyan-400 animate-wallAnimation"
+    }
+
     return classname
   }
 
@@ -53,15 +57,15 @@ function Grid(props) {
             <div className='flex flex-row' key={`row-${rowIndex}`}>
               {
                 row.map((node, colIndex) => {
-                  return (                    
-                    <div 
+                  return (
+                    <div
                       key={`node-${rowIndex}-${colIndex}`}
-                      className={getClassName(node)} 
-                      onMouseDown={()=>{handleMouseDown(node)}} 
-                      onMouseUp={handleMouseUp} 
-                      onMouseOver={()=>{handleMouseOver(node)}} 
+                      className={getClassName(node)}
+                      onMouseDown={() => { handleMouseDown(node) }}
+                      onMouseUp={handleMouseUp}
+                      onMouseOver={() => { handleMouseOver(node) }}
                     />
-                  )
+                  );
                 })
               }
             </div>
@@ -69,7 +73,7 @@ function Grid(props) {
         })
       }
     </div>
-  ) 
+  )
 }
 
 export default Grid
--- src/components/Pathfinder.jsx
+++ src/components/Pathfinder.jsx
@@ -1,36 +1,44 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 //components
 import Grid from './Grid'
+
+
+// graph
+import { Graph, BFS } from '../lib';
+
+
 function Pathfinder() {
     //grid
     const [grid, setGrid] = useState({
         nodes: [],
-        startNode: [0,0],
-        endNode:[0,0],
-        nodeWidth:25,
-        nodeHeight:25,
-        rows:0,
-        cols:0
+        startNode: `${0}-${0}`,
+        endNode: `${0}-${0}`,
+        nodeWidth: 25,
+        nodeHeight: 25,
+        rows: 0,
+        cols: 0,
     })
 
-    function randomXY(row,col){
-        const x = Math.floor(Math.random()*row)
-        const y = Math.floor(Math.random()*col)
-        return [x,y]
+    const [gridIsSet, setGridIsSet] = useState(false);
+
+    function randomXY(row, col) {
+        const x = Math.floor(Math.random() * row)
+        const y = Math.floor(Math.random() * col)
+        return [x, y]
     }
-    function populateGrid(row,col,startX,startY,endX,endY){
-        
-        var count =0
+    function populateGrid(row, col, startX, startY, endX, endY) {
+
+        var count = 0
         const nodes = []
-        for(let i=0;i<row;i++){
+        for (let i = 0; i < row; i++) {
 
             const currentRow = []
-            for(let j=0;j<col;j++){
+            for (let j = 0; j < col; j++) {
                 count++
-               currentRow.push({
-                    x:i,
-                    y:j,
+                currentRow.push({
+                    x: i,
+                    y: j,
                     isStartNode: i === startX && j === startY,
                     isEndNode: i === endX && j === endY,
                     isWall: false,
@@ -49,38 +57,94 @@ function Pathfinder() {
         function handleResize() {
             const width = window.innerWidth;
             const height = window.innerHeight;
-            const row = Math.max(Math.floor(height/40)-3,3)
-            const col = Math.max(Math.floor(width/40)-2,3)
-            const [startX,startY] = randomXY(row,col)
-            const [endX,endY] = randomXY(row,col)
-            const initialGrid = populateGrid(row,col,startX,startY,endX,endY)
+            const row = 5;
+            const col = 5;
+            // const row = Math.max(Math.floor(height / 40) - 3, 3)
+            // const col = Math.max(Math.floor(width / 40) - 2, 3)
+            const [startX, startY] = randomXY(row, col)
+            const [endX, endY] = randomXY(row, col)
+            const initialGrid = populateGrid(row, col, startX, startY, endX, endY)
             // console.log(startX,startY,endX,endY)
             setGrid({
                 ...grid,
                 rows: row,
                 cols: col,
                 nodes: initialGrid,
-                startNode: [startX,startY],
-                endNode: [endX,endY],
+                startNode: `${startX}-${startY}`,
+                endNode: `${endX}-${endY}`,
             })
+            setGridIsSet(true);
         }
         handleResize()
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
     }, []);
-    
-  return (
-    <div>
-        <Grid 
-            grid={grid.nodes}
-            setGrid={setGrid}
-            rows={grid.rows} 
-            cols={grid.cols} 
-            nodeHeight= {grid.nodeHeight} 
-            nodeWidth={grid.nodeWidth}
-        />
-    </div>
-  )
+
+    const generateAdjacencyList = (grid) => {
+        const adjacencyList = {};
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i].length; j++) {
+                const node = grid[i][j];
+                const { x, y } = node;
+                const key = `${x}-${y}`;
+                const neighbors = [];
+                if (i > 0) neighbors.push(`${x - 1}-${y}`);
+                if (i < grid.length - 1) neighbors.push(`${x + 1}-${y}`);
+                if (j > 0) neighbors.push(`${x}-${y - 1}`);
+                if (j < grid[i].length - 1) neighbors.push(`${x}-${y + 1}`);
+                adjacencyList[key] = neighbors;
+            }
+        }
+        return adjacencyList;
+    }
+
+    const generateGraph = (grid) => {
+        const adjacencyList = generateAdjacencyList(grid)
+        const graph = new Graph()
+        graph.adjacencyList = adjacencyList;
+        return graph;
+    }
+
+    const getNodesToColorFromBFS = (addedToQueueBy, endNode) => {
+        const nodesToColor = [];
+        let currentNode = endNode;
+        while (currentNode) {
+            nodesToColor.unshift(currentNode);
+            currentNode = addedToQueueBy[currentNode];
+        }
+        return nodesToColor;
+    }
+
+
+    if (gridIsSet) {
+        const visitedNodes = getNodesToColorFromBFS(
+            BFS(generateGraph(grid.nodes).adjacencyList, grid.startNode, grid.endNode).addedToQueueBy, grid.endNode
+        )
+        const newGrid = grid.nodes.map((row, i) => {
+            return row.map((node, j) => {
+                const nodeKey = `${node.x}-${node.y}`;
+                const isVisited = visitedNodes.includes(nodeKey);
+                return { ...node, isVisited };
+            });
+        });
+        setGrid({ ...grid, nodes: newGrid })
+        setGridIsSet(false);
+    }
+
+
+
+    return (
+        <div>
+            <Grid
+                grid={grid.nodes}
+                setGrid={setGrid}
+                rows={grid.rows}
+                cols={grid.cols}
+                nodeHeight={grid.nodeHeight}
+                nodeWidth={grid.nodeWidth}
+            />
+        </div>
+    )
 }
 
 export default Pathfinder
